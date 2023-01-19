d6999718d313c61df92fd14054b799f51591312c
Marko Vukušić <marko@codesplitters.com>
2023-01-19 14:52:15 +0100

    Create composite loaders

--- EssentialApp/SceneDelegate.swift
+++ EssentialApp/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 import EssentialFeed
 import EssentialFeediOS
 
@@ -18,12 +19,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
         
         let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-        
         let session = URLSession(configuration: .ephemeral)
         let client = URLSessionHTTPClient(session: session)
-        let feedLoader = RemoteFeedLoader(url: url, client: client)
-        let imageLoader = RemoteFeedImageDataLoader(client: client)
-        let feedViewController = FeedUIComposer.feedComposedWith(feedLoader: feedLoader, imageLoader: imageLoader)
+        
+        let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feed-store.sqlite")
+        let coreDataFeedStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+        
+        let localFeedLoader = LocalFeedLoader(store: coreDataFeedStore, currentDate: Date.init)
+        let remoteFeedLoader = RemoteFeedLoader(url: url, client: client)
+        let feedLoaderComposite = FeedLoaderWithFallbackComposite(primary: remoteFeedLoader, fallback: localFeedLoader)
+        
+        let localImageLoader = LocalFeedImageDataLoader(store: coreDataFeedStore)
+        let remoteImageLoader = RemoteFeedImageDataLoader(client: client)
+        let imageLoaderComposite = FeedImageDataLoaderWithFallbackComposite(primary: localImageLoader, fallback: remoteImageLoader)
+        
+        let feedViewController = FeedUIComposer.feedComposedWith(feedLoader: feedLoaderComposite, imageLoader: imageLoaderComposite)
         
         window?.rootViewController = feedViewController
     }
