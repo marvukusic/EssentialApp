ce299636e48ea466d141b75c6c0b22c21b22e62c
Marko Vukušić <marko@codesplitters.com>
2023-01-23 11:28:54 +0100

    Extract FeedLoaderStub into a shared scope to remove duplication

--- EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -59,16 +59,4 @@ final class FeedLoaderCacheDecoratorTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
     }
-    
-    class FeedLoaderStub: FeedLoader {
-        let result: FeedLoader.Result
-        
-        init(result: FeedLoader.Result) {
-            self.result = result
-        }
-        
-        func load(completion: @escaping (FeedLoader.Result) -> Void) {
-            completion(result)
-        }
-    }
 }
