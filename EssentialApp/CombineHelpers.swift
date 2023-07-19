7b989b54f9ad14279c030377cf1e0ee1300c0312
Marko Vukušić <marko@codesplitters.com>
2023-07-19 14:14:15 +0200

    Make LocalFeedLoader.load sync

--- EssentialApp/CombineHelpers.swift
+++ EssentialApp/CombineHelpers.swift
@@ -40,7 +40,9 @@ public extension LocalFeedLoader {
     
     func loadPublisher() -> Publisher {
         Deferred {
-            Future(self.load)
+            Future { completion in
+                completion(Result{ try self.load() })
+            }
         }.eraseToAnyPublisher()
     }
 }
